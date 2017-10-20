3924ea1815f63ae024de0926b88d57b05ff5e180
Asylgareev Arthur <son.of.gallifrei@gmail.com>
2017-10-20 22:56:14 +0500

    Custom message and emptiness checks were added
    1. The ability to add custom EnsureException message was added
    2. IsEmty and IsNotEmpty checks for collections were added

--- Ensure.Core.Ensure/Ensure.Rules.cs
+++ Ensure.Core.Ensure/Ensure.Rules.cs
@@ -1,6 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
-
+using System.Linq;
 
 namespace Ensure.Core.Ensure
 {
@@ -14,11 +15,12 @@ namespace Ensure.Core.Ensure
         /// </summary>
         /// <param name="valueName">Name of the variable being checked.</param>
         /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="customMessage">Custom validation ErrorMessage.</param>
         /// <param name="parentType">Type of the class which contains the variable as property.</param>
         /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when value is nonpositive</exception> 
-        public static void IsPositive(string valueName, double value, Type parentType = null)
+        public static void IsPositive(string valueName, double value, string customMessage = null, Type parentType = null)
         {
-            PerformEnsureCheck(valueName, value, (v) => v > 0, "Must be positive", parentType);
+            PerformEnsureCheck(valueName, value, (v) => v > 0, customMessage ?? "Must be positive", parentType);
         }
 
         /// <summary>
@@ -26,11 +28,12 @@ namespace Ensure.Core.Ensure
         /// </summary>
         /// <param name="valueName">Name of the variable being checked.</param>
         /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="customMessage">Custom validation ErrorMessage.</param>
         /// <param name="parentType">Type of the class which contains the variable as property.</param>
         /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when value is nonnegative</exception> 
-        public static void IsNegative(string valueName, double value, Type parentType = null)
+        public static void IsNegative(string valueName, double value, string customMessage = null, Type parentType = null)
         {
-            PerformEnsureCheck(valueName, value, (v) => v < 0, "Must be negative", parentType);
+            PerformEnsureCheck(valueName, value, (v) => v < 0, customMessage ?? "Must be negative", parentType);
         }
 
         /// <summary>
@@ -38,11 +41,12 @@ namespace Ensure.Core.Ensure
         /// </summary>
         /// <param name="valueName">Name of the variable being checked.</param>
         /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="customMessage">Custom validation ErrorMessage.</param>
         /// <param name="parentType">Type of the class which contains the variable as property.</param>
         /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when value is negative</exception> 
-        public static void IsNonNegative(string valueName, double value, Type parentType = null)
+        public static void IsNonNegative(string valueName, double value, string customMessage = null, Type parentType = null)
         {
-            PerformEnsureCheck(valueName, value, (v) => v >= 0, "Must be nonnegative", parentType);
+            PerformEnsureCheck(valueName, value, (v) => v >= 0, customMessage ?? "Must be nonnegative", parentType);
         }
 
         /// <summary>
@@ -50,11 +54,12 @@ namespace Ensure.Core.Ensure
         /// </summary>
         /// <param name="valueName">Name of the variable being checked.</param>
         /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="customMessage">Custom validation ErrorMessage.</param>
         /// <param name="parentType">Type of the class which contains the variable as property.</param>
         /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when value is positive</exception> 
-        public static void IsNonPositive(string valueName, double value, Type parentType = null)
+        public static void IsNonPositive(string valueName, double value, string customMessage = null, Type parentType = null)
         {
-            PerformEnsureCheck(valueName, value, (v) => v < 0, "Must be nonpositive", parentType);
+            PerformEnsureCheck(valueName, value, (v) => v < 0, customMessage ?? "Must be nonpositive", parentType);
         }
 
         /// <summary>
@@ -62,11 +67,12 @@ namespace Ensure.Core.Ensure
         /// </summary>
         /// <param name="valueName">Name of the variable being checked.</param>
         /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="customMessage">Custom validation ErrorMessage.</param>
         /// <param name="parentType">Type of the class which contains the variable as property.</param>
         /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when input string is null or empty</exception> 
-        public static void IsNotNullOrEmpty(string valueName, string value, Type parentType = null)
+        public static void IsNotNullOrEmpty(string valueName, string value, string customMessage = null, Type parentType = null)
         {
-            PerformEnsureCheck(valueName, value, (v) => !string.IsNullOrEmpty(value), "String must not be null or empty", parentType);
+            PerformEnsureCheck(valueName, value, (v) => !string.IsNullOrEmpty(value), customMessage ?? "String must not be null or empty", parentType);
         }
 
         /// <summary>
@@ -74,11 +80,12 @@ namespace Ensure.Core.Ensure
         /// </summary>
         /// <param name="valueName">Name of the variable being checked.</param>
         /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="customMessage">Custom validation ErrorMessage.</param>
         /// <param name="parentType">Type of the class which contains the variable as property.</param>
         /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when input file does not exists</exception> 
-        public static void FileExists(string valueName, string value, Type parentType = null)
+        public static void FileExists(string valueName, string value, string customMessage = null, Type parentType = null)
         {
-            PerformEnsureCheck(valueName, value, (v) => File.Exists(value), "File must exist", parentType);
+            PerformEnsureCheck(valueName, value, (v) => File.Exists(value), customMessage ?? "File must exist", parentType);
         }
 
         /// <summary>
@@ -86,11 +93,38 @@ namespace Ensure.Core.Ensure
         /// </summary>
         /// <param name="valueName">Name of the variable being checked.</param>
         /// <param name="value">Actual value of the variable being checked.</param>
+        /// <param name="customMessage">Custom validation ErrorMessage.</param>
         /// <param name="parentType">Type of the class which contains the variable as property.</param>
         /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when input variable is null</exception> 
-        public static void IsNotNull<T>(string valueName, T value, Type parentType = null) where T : class
+        public static void IsNotNull<T>(string valueName, T value, string customMessage = null, Type parentType = null) where T : class
         {
-            PerformEnsureCheck(valueName, value, (v) => v != null, "Variable must not be null", parentType);
+            PerformEnsureCheck(valueName, value, (v) => v != null, customMessage ?? "Variable must not be null", parentType);
+        }
+
+        /// <summary>
+        /// This function ensures that the input collection is not Empty
+        /// </summary>
+        /// <param name="collectionName">Name of the variable being checked.</param>
+        /// <param name="collection">Actual value of the variable being checked.</param>
+        /// <param name="customMessage">Custom validation ErrorMessage.</param>
+        /// <param name="parentType">Type of the class which contains the variable as property.</param>
+        /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when input variable is null</exception> 
+        public static void IsNotEmpty<T>(string collectionName, IEnumerable<T> collection, string customMessage = null, Type parentType = null)
+        {
+            PerformEnsureCheck(collectionName, collection, (v) => v.Any(), customMessage ?? "Collection must not be empty", parentType);
+        }
+
+                /// <summary>
+        /// This function ensures that the input collection is not Empty
+        /// </summary>
+        /// <param name="collectionName">Name of the variable being checked.</param>
+        /// <param name="collection">Actual value of the variable being checked.</param>
+        /// <param name="customMessage">Custom validation ErrorMessage.</param>
+        /// <param name="parentType">Type of the class which contains the variable as property.</param>
+        /// <exception cref="TextGameFramework.Ensure.EnsureException">Thrown when input variable is null</exception> 
+        public static void IsEmpty<T>(string collectionName, IEnumerable<T> collection, string customMessage = null, Type parentType = null)
+        {
+            PerformEnsureCheck(collectionName, collection, (v) => !v.Any(), customMessage ?? "Collection must be empty", parentType);
         }
     }
 }
